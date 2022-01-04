14f1c334a47c11b3f23677c58856caade89ce36b
yuutan1017 <n.you0611@gmail.com>
2022-01-04 19:44:21 +0900

    create post page

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,14 +1,22 @@
 import Layout from "../components/Layout";
+import { getAllPostsData } from "../lib/posts";
+import Post from "../components/Post";
 
-const Blog = () => {
+const Blog = ({posts}) => {
   return (
     <Layout title="Blog">
-      <div className="text-4xl">
-        Blog page
-      </div>
+      <ul className="m-10">
+        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
     </Layout>
   )
 }
 
 export default Blog;
 
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  return {
+    props: {posts},
+  };
+}
