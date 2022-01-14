7e4ad1369cb739b53f04bc4b2381aabd80046a45
yuutan1017 <n.you0611@gmail.com>
2022-01-14 16:35:57 +0900

    motionどうすんの

---
+++ components/HomePage.js
@@ -0,0 +1,9 @@
+
+export default function HomePage() {
+    return (
+        <>
+        <h1 className="text-5xl font-sans my-3">Thanks for visiting!!</h1>
+        <p className="text-3xl font-sans my-3">Ylog</p>
+        </>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Layout from "../components/Layout";
+import HomePage from "../components/HomePage";
 
-export default function Home() {
+
+export default function homePage() {
   return (
     <Layout title="Home">
-      <h1 className="text-5xl font-sans my-3">Thanks for visiting!!</h1>
-      <p className="text-3xl font-sans my-3">Ylog</p>
+      <HomePage />
     </Layout>
   );
 }
