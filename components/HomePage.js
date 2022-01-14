7e9a46e7eacda2b2a91caa13f6c2dbabe18ea18c
yuutan1017 <n.you0611@gmail.com>
2022-01-14 16:43:12 +0900

    textMotion

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,9 +1,20 @@
+import React from "react";
+import { motion } from "framer-motion";
+
 
 export default function HomePage() {
     return (
-        <>
-        <h1 className="text-5xl font-sans my-3">Thanks for visiting!!</h1>
-        <p className="text-3xl font-sans my-3">Ylog</p>
-        </>
+    <motion.h1
+        className="text-center text-5xl font-sans my-3"
+        animate={{ 
+            x: [0, 100, 0]
+        }}
+        transition={{
+            duration: 2,
+            times: [0, 0.2, 1]
+        }}
+    >
+        Thanks for visiting!!
+    </motion.h1>
     );
 }
