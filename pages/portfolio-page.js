7fb0becf65242a07379c22cbc5326992ef55d0e9
yuutan1017 <n.you0611@gmail.com>
2022-01-17 16:48:22 +0900

    endsec1

--- pages/portfolio-page.js
+++ pages/portfolio-page.js
@@ -1,28 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from 'framer-motion'
 
 import Layout from '../components/Layout'
 
-const Portfolio = () => {
+
+
+function Portfolio() {
+
+    const [isAnimating, setIsAnimating] = useState(false);
+
     return (
         <Layout title='Portfolio' className="p-0 box-border">
-            <motion.div 
+            <motion.div
                 className='w-48 h-48 bg-green-400 flex flex-col items-center justify-center mr-auto ml-20'
                 animate={{
-                    x: "50rem",
-                    opacity: 1,
+                    x: isAnimating ? 900 : 0,
+                    opacity: isAnimating ? 1 : 0.5,
+                    rotate: isAnimating ? 270 : 0,
                 }}
                 initial={{
-                    opacity: 0.1,
+                    opacity: 0,
                 }}
                 transition={{
                     type: "spring",
                     stifiness: 500,
                 }}
+                onClick={() => setIsAnimating(!isAnimating)}
             >
             </motion.div>
         </Layout>
-    )
+    );
 }
 
 export default Portfolio;
