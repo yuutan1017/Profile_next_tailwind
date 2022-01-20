4d05f6a22910833265064db1cf1bff166752087f
yuutan1017 <n.you0611@gmail.com>
2022-01-20 20:26:27 +0900

    create-skills-page

--- components/box.jsx
+++ components/box.jsx
@@ -53,3 +53,52 @@ dragConstraints={{
 </motion.div> */}
 
 // export default Portfolio;
+
+
+//時間差で子要素を表示
+// function Portfolio() {
+
+//     const boxVariant = {
+//         hidden: {
+//             x: "-100vh",
+//         },
+//         visible: {
+//             x : 0,
+//             transition: {
+//                 delay: 0.5,
+//                 when: "beforeChildren",
+//                 staggerChildren: 0.2
+//             },
+//         },
+//     }
+
+//     const listVariant = {
+//         hidden: {
+//             x: -10,
+//             opacity: 0,
+//         },
+//         visible: {
+//             x : 0,
+//             opacity: 1,
+//         },
+//     }
+
+//     return (
+//         <Layout title='Portfolio' className="p-0 box-border">
+//             <motion.div
+//                 className='w-48 h-48 bg-green-400 flex flex-col items-center justify-center mr-auto ml-20'
+//                 variants={boxVariant}
+//                 animate="visible"
+//                 initial="hidden"
+//             >
+//                 {[1,2,3].map(box => {
+//                     return (
+//                     <motion.li 
+//                         className="w-1 h-1 bg-white p-3 m-2 list-none"
+//                         variants={listVariant}
+//                     ></motion.li>
+//                 )})}
+//             </motion.div>
+//         </Layout>
+//     );
+// }
---
+++ pages/skills-page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Skills() {
+    return (
+        <div className='text-center'>
+            skills_page
+        </div>
+    )
+}
